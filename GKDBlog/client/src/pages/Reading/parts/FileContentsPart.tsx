5b5b87fd628cf8f1dd71f27ecef8681989a51f15
unknown <dudgns113@gmail.com>
2025-06-09 19:55:47 +0900

    25.06.09 FIX: 마크다운 +, -, * 구분

--- GKDBlog/client/src/pages/Reading/parts/FileContentsPart.tsx
+++ GKDBlog/client/src/pages/Reading/parts/FileContentsPart.tsx
@@ -65,6 +65,21 @@ export const FileContentsPart: FC<FileContentsPartProps> = ({className, style, .
           </code>
         )
       }
+    },
+
+    li({children, node, ...props}) {
+      const raw = node?.position?.start?.line ? stringArr[node?.position?.start?.line - 1] : ''
+      let markerStyle = '•' // 기본 점
+
+      if (raw.trimStart().startsWith('+')) markerStyle = '🔹'
+      else if (raw.trimStart().startsWith('*')) markerStyle = '✔️'
+
+      return (
+        <li {...props} style={{listStyleType: 'none'}}>
+          <span style={{marginRight: '0.5em'}}>{markerStyle}</span>
+          {children}
+        </li>
+      )
     }
   }
 
