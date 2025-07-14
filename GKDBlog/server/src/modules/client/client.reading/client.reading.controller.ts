d2a4b2743b2fd1ba0e9c0f99aa356faab3e50a9a
unknown <dudgns113@gmail.com>
2025-07-14 14:21:24 +0900

    25.07.14 Swagger 적용
    :page_facing_up: [server] Swagger 적용
    
    :memo: [server] file.entity 에 contentsArr 부분 주석 추가

--- GKDBlog/server/src/modules/client/client.reading/client.reading.controller.ts
+++ GKDBlog/server/src/modules/client/client.reading/client.reading.controller.ts
@@ -1,9 +1,11 @@
 import {Body, Controller, Delete, Get, Headers, Param, Post, Put, UseGuards} from '@nestjs/common'
 import {ClientReadingService} from './client.reading.service'
 import {CheckJwtValidationGuard} from '@common/guards/guards.checkJwtValidation'
+import {ApiBody, ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger'
 
 import * as HTTP from '@common/types/httpDataTypes'
 
+@ApiTags('Client Reading')
 @Controller('client/reading')
 export class ClientReadingController {
   constructor(private readonly clientService: ClientReadingService) {}
@@ -11,6 +13,18 @@ export class ClientReadingController {
   // POST AREA:
   @Post('/addComment')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '댓글 추가', description: '댓글 추가'})
+  @ApiBody({
+    schema: {
+      properties: {
+        content: {type: 'string', example: 'test'},
+        fileOId: {type: 'string', example: '123456781234567812345678'},
+        userOId: {type: 'string', example: '123456781234567812345678'}
+      }
+    }
+  })
+  @ApiResponse({status: 200, description: '댓글 추가 성공'})
+  @ApiResponse({status: 400, description: '댓글 추가 실패'})
   async addComment(@Headers() headers: any, @Body() data: HTTP.AddCommentDataType) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.addComment(jwtPayload, data)
@@ -19,6 +33,18 @@ export class ClientReadingController {
 
   @Post('/addReply')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '대댓글 추가', description: '대댓글 추가'})
+  @ApiBody({
+    schema: {
+      properties: {
+        commentOId: {type: 'string', example: '123456781234567812345678'},
+        content: {type: 'string', example: 'test'},
+        fileOId: {type: 'string', example: '123456781234567812345678'},
+        userOId: {type: 'string', example: '123456781234567812345678'}
+      }
+    }
+  })
+  @ApiResponse({status: 200, description: '대댓글 추가 성공'})
   async addReply(@Headers() headers: any, @Body() data: HTTP.AddReplyDataType) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.addReply(jwtPayload, data)
@@ -29,6 +55,18 @@ export class ClientReadingController {
 
   @Put('/deleteReply')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '대댓글 삭제', description: '대댓글 삭제'})
+  @ApiBody({
+    schema: {
+      properties: {
+        commentOId: {type: 'string', example: '123456781234567812345678'},
+        dateString: {type: 'string', example: '2025-01-01'},
+        userOId: {type: 'string', example: '123456781234567812345678'}
+      }
+    }
+  })
+  @ApiResponse({status: 200, description: '대댓글 삭제 성공'})
+  @ApiResponse({status: 400, description: '대댓글 삭제 실패'})
   async deleteReply(@Headers() headers: any, @Body() data: HTTP.DeleteReplyDataType) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.deleteReply(jwtPayload, data)
@@ -37,6 +75,17 @@ export class ClientReadingController {
 
   @Put(`/modifyComment`)
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '댓글 수정', description: '댓글 수정'})
+  @ApiBody({
+    schema: {
+      properties: {
+        commentOId: {type: 'string', example: '123456781234567812345678'},
+        content: {type: 'string', example: 'test'}
+      }
+    }
+  })
+  @ApiResponse({status: 200, description: '댓글 수정 성공'})
+  @ApiResponse({status: 400, description: '댓글 수정 실패'})
   async modifyComment(@Headers() headers: any, @Body() data: HTTP.ModifyCommentDataType) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.modifyComment(jwtPayload, data)
@@ -45,6 +94,19 @@ export class ClientReadingController {
 
   @Put('/modifyReply')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '대댓글 수정', description: '대댓글 수정'})
+  @ApiBody({
+    schema: {
+      properties: {
+        commentOId: {type: 'string', example: '123456781234567812345678'},
+        content: {type: 'string', example: 'test'},
+        dateString: {type: 'string', example: '2025-01-01'},
+        userOId: {type: 'string', example: '123456781234567812345678'}
+      }
+    }
+  })
+  @ApiResponse({status: 200, description: '대댓글 수정 성공'})
+  @ApiResponse({status: 400, description: '대댓글 수정 실패'})
   async modifyReply(@Headers() headers: any, @Body() data: HTTP.ModifyReplyDataType) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.modifyReply(jwtPayload, data)
@@ -54,6 +116,9 @@ export class ClientReadingController {
   //  GET AREA:
   @Get('/readCommentsArr/:fileOId')
   // @UseGuards(CheckJwtValidationGuard) // 회원 아니어도 읽을 수 있게 할까...?
+  @ApiOperation({summary: '댓글 목록 조회', description: '댓글 목록 조회'})
+  @ApiResponse({status: 200, description: '댓글 목록 조회 성공'})
+  @ApiResponse({status: 400, description: '댓글 목록 조회 실패'})
   async readCommentsArr(@Param('fileOId') fileOId: string) {
     const {ok, body, errObj} = await this.clientService.readCommentsArr(fileOId)
     return {ok, body, errObj}
@@ -61,6 +126,9 @@ export class ClientReadingController {
 
   @Get('/readFile/:fileOId')
   // @UseGuards(CheckJwtValidationGuard) // 회원 아니어도 읽을 수 있게 할까...??
+  @ApiOperation({summary: '파일 조회', description: '파일 조회'})
+  @ApiResponse({status: 200, description: '파일 조회 성공'})
+  @ApiResponse({status: 400, description: '파일 조회 실패'})
   async readFile(@Param('fileOId') fileOId: string) {
     /**
      * 파일이 숨겨졌거나 공지로 등록되어있으면 errObj.isHidden 이 true 이다.
@@ -72,6 +140,9 @@ export class ClientReadingController {
 
   @Get('/readFileHidden/:fileOId')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '숨김 파일 조회', description: '숨김 파일 조회'})
+  @ApiResponse({status: 200, description: '숨김 파일 조회 성공'})
+  @ApiResponse({status: 400, description: '숨김 파일 조회 실패'})
   async readFileHidden(@Headers() headers: any, @Param('fileOId') fileOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.readFileHidden(jwtPayload, fileOId)
@@ -81,6 +152,9 @@ export class ClientReadingController {
   // DELETE AREA:
   @Delete('/deleteComment/:commentOId')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '댓글 삭제', description: '댓글 삭제'})
+  @ApiResponse({status: 200, description: '댓글 삭제 성공'})
+  @ApiResponse({status: 400, description: '댓글 삭제 실패'})
   async deleteComment(@Headers() headers: any, @Param('commentOId') commentOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.deleteComment(jwtPayload, commentOId)
--- GKDBlog/server/src/modules/client/client.userInfo/client.userInfo.controller.ts
+++ GKDBlog/server/src/modules/client/client.userInfo/client.userInfo.controller.ts
@@ -1,8 +1,9 @@
 import {Body, Controller, Delete, Get, Headers, Param, Post, Put, UseGuards} from '@nestjs/common'
 import {ClientUserInfoService} from './client.userInfo.service'
 import {CheckJwtValidationGuard} from '@common/guards/guards.checkJwtValidation'
-import * as HTTP from '@common/types/httpDataTypes'
+import {ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger'
 
+@ApiTags('Client UserInfo')
 @Controller('client/userInfo')
 export class ClientUserInfoController {
   constructor(private readonly clientService: ClientUserInfoService) {}
@@ -10,6 +11,9 @@ export class ClientUserInfoController {
   // PUT AREA:
   @Put('/openUserChatRoom/:targetUserOId')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '유저 채팅방 열기', description: '유저 채팅방 열기'})
+  @ApiResponse({status: 200, description: '유저 채팅방 열기 성공'})
+  @ApiResponse({status: 400, description: '유저 채팅방 열기 실패'})
   async openUserChatRoom(@Headers() headers: any, @Param('targetUserOId') targetUserOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.openUserChatRoom(jwtPayload, targetUserOId)
@@ -19,6 +23,9 @@ export class ClientUserInfoController {
   // GET AREA:
   @Get('/getChatArr/:chatRoomOId/:firstIndex')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '채팅 목록 조회', description: '채팅 목록 조회'})
+  @ApiResponse({status: 200, description: '채팅 목록 조회 성공'})
+  @ApiResponse({status: 400, description: '채팅 목록 조회 실패'})
   async getChatArr(@Headers() headers: any, @Param('chatRoomOId') chatRoomOId: string, @Param('firstIndex') firstIndex: number) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.getChatArr(jwtPayload, chatRoomOId, firstIndex)
@@ -27,6 +34,9 @@ export class ClientUserInfoController {
 
   @Get('/getChatRoom/:chatRoomOId')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '채팅방 조회', description: '채팅방 조회'})
+  @ApiResponse({status: 200, description: '채팅방 조회 성공'})
+  @ApiResponse({status: 400, description: '채팅방 조회 실패'})
   async getChatRoom(@Headers() headers: any, @Param('chatRoomOId') chatRoomOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.getChatRoom(jwtPayload, chatRoomOId)
@@ -35,6 +45,9 @@ export class ClientUserInfoController {
 
   @Get('/getChatRoomRow/:userOId/:chatRoomOId')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '채팅방 행 조회', description: '채팅방 행 조회'})
+  @ApiResponse({status: 200, description: '채팅방 행 조회 성공'})
+  @ApiResponse({status: 400, description: '채팅방 행 조회 실패'})
   async getChatRoomRow(@Headers() headers: any, @Param('userOId') userOId: string, @Param('chatRoomOId') chatRoomOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.getChatRoomRow(jwtPayload, userOId, chatRoomOId)
@@ -43,6 +56,9 @@ export class ClientUserInfoController {
 
   @Get('/getChatRoomRowArr/:userOId')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '채팅방 행 목록 조회', description: '채팅방 행 목록 조회'})
+  @ApiResponse({status: 200, description: '채팅방 행 목록 조회 성공'})
+  @ApiResponse({status: 400, description: '채팅방 행 목록 조회 실패'})
   async getChatRoomRowArr(@Headers() headers: any, @Param('userOId') userOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.getChatRoomRowArr(jwtPayload, userOId)
@@ -51,6 +67,9 @@ export class ClientUserInfoController {
 
   @Get('/getNewAlarmArrLen/:userOId')
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '새 알림 목록 길이 조회', description: '새 알림 목록 길이 조회'})
+  @ApiResponse({status: 200, description: '새 알림 목록 길이 조회 성공'})
+  @ApiResponse({status: 400, description: '새 알림 목록 길이 조회 실패'})
   async getNewAlarmArrLen(@Headers() headers: any, @Param('userOId') userOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.getNewAlarmArrLen(jwtPayload, userOId)
@@ -59,6 +78,9 @@ export class ClientUserInfoController {
 
   @Get(`/refreshAlarmArr/:userOId`)
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '알림 목록 새로고침', description: '알림 목록 새로고침'})
+  @ApiResponse({status: 200, description: '알림 목록 새로고침 성공'})
+  @ApiResponse({status: 400, description: '알림 목록 새로고침 실패'})
   async refreshAlarmArr(@Headers() headers: any, @Param('userOId') userOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.refreshAlarmArr(jwtPayload, userOId)
@@ -69,6 +91,9 @@ export class ClientUserInfoController {
 
   @Delete(`/deleteAlarm/:alarmOId`)
   @UseGuards(CheckJwtValidationGuard)
+  @ApiOperation({summary: '알림 삭제', description: '알림 삭제'})
+  @ApiResponse({status: 200, description: '알림 삭제 성공'})
+  @ApiResponse({status: 400, description: '알림 삭제 실패'})
   async deleteAlarm(@Headers() headers: any, @Param('alarmOId') alarmOId: string) {
     const {jwtFromServer, jwtPayload} = headers
     const {ok, body, errObj} = await this.clientService.deleteAlarm(jwtPayload, alarmOId)
