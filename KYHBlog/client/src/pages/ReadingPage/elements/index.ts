77770b10f2055d5c39bcb59744dd1850d580053e
unknown <dudgns113@gmail.com>
2025-09-11 20:56:55 +0900

    25.09.11 대댓글 작성 유저한테 채팅 가능하도록 수정
    :wrench: [Client] 대댓글 작성 유저한테도 채팅 할 수 있도록 수정

--- KYHBlog/client/src/pages/ReadingPage/elements/index.ts
+++ KYHBlog/client/src/pages/ReadingPage/elements/index.ts
@@ -1,1 +1,2 @@
 export * from './CommentUserInfoE'
+export * from './ReplyUserInfoE'
--- KYHBlog/client/src/pages/ReadingPage/featuresReply/ReplyUserNameF.tsx
+++ KYHBlog/client/src/pages/ReadingPage/featuresReply/ReplyUserNameF.tsx
@@ -1,14 +1,19 @@
 import {useCallback} from 'react'
+import {useFileCallbacksContext, useFileStatesContext} from '@context'
+import {ReplyUserInfoE} from '../elements'
 
 import type {FC, MouseEvent} from 'react'
 import type {DivCommonProps} from '@prop'
 import type {ReplyType} from '@shareType'
-import {useFileCallbacksContext} from '@context'
 
 type ReplyUserNameFProps = DivCommonProps & {reply: ReplyType}
 
 export const ReplyUserNameF: FC<ReplyUserNameFProps> = ({reply, className, style, ...props}) => {
+  const {replyOId_user} = useFileStatesContext()
   const {selectReplyUser} = useFileCallbacksContext()
+
+  const isUserSelected = replyOId_user === reply.replyOId
+
   const onClickUserName = useCallback(
     (reply: ReplyType) => (e: MouseEvent<HTMLParagraphElement>) => {
       e.preventDefault()
@@ -23,6 +28,7 @@ export const ReplyUserNameF: FC<ReplyUserNameFProps> = ({reply, className, style
       <p className="_replyUserName" onClick={onClickUserName(reply)}>
         {reply.userName}
       </p>
+      {isUserSelected && <ReplyUserInfoE reply={reply} />}
     </div>
   )
 }
